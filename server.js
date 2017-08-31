56c097b074c11f7ca1ebea6d93485d3764612638
Lavan-daitha <30766159+Lavan-daitha@users.noreply.github.com>
2017-09-01 00:12:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.get('/page1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
 });
 
-app.get('/page2.html', function (req, res) {
+app.get('/page2', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'page2.html'));
 });
 
-app.get('/page3.html', function (req, res) {
+app.get('/page3', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'page3.html'));
 });
 
