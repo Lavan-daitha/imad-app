53310dd56667629077929255be272a5ffd354e64
Lavan-daitha <30766159+Lavan-daitha@users.noreply.github.com>
2017-09-02 11:59:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,9 +11,9 @@ button.onclick=function(){
     
    request.onreadystatechange=function(){
      
-     if(request.readystate==XMLHttpReqest.DONE) 
+     if(request.readystate===XMLHttpReqest.DONE) 
      {
-         if(request.status==200)
+         if(request.status===200)
          {
              var counter=request.responseText;
              var span=document.getElementById('count');
